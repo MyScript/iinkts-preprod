50f604f300668e3805948f4dc80d88f51a2ccdef
Jocelyn Boureau <jocelyn.boureau@myscript.com>
2026-06-29 20:13:54 +0200

    docs(chore) add missing docs groups

--- src/symbol/edge/index.ts
+++ src/symbol/edge/index.ts
@@ -1,14 +1,7 @@
-import type { TEdgeArc } from "./Arc"
-import type { TEdgeLine } from "./Line"
-import type { TEdgePolyLine } from "./PolyLine"
-
+export * from "./Anchor"
 export * from "./Edge-enum"
 export * from "./Edge"
 export * from "./Arc"
 export * from "./Line"
 export * from "./PolyLine"
-  
-/**
- * @group Symbol
- */
-export type TEdge = TEdgeArc | TEdgeLine | TEdgePolyLine
+
